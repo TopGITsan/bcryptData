a6e68cde6ce10a9e14fbe11b6c94752adde39552
TopGitSan <c.toparcean@gmail.com>
2019-04-16 22:13:23 +0200

    use async and sync hash

--- server.js
+++ server.js
@@ -15,15 +15,22 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword,saltRounds, (err,hash)=>{
+    console.log(hash);
 
+    bcrypt.compare(myPlaintextPassword, hash, (err, res)=>{
+        console.log(res);
+    });
+});
 
 
 //END_ASYNC
 
 //START_SYNC
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 
-
-
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(hash, result);
 //END_SYNC
 
 
